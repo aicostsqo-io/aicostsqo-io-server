70c7802ac3a53d92f35b1619998ebd620270fee9
mustafahincal <mustafahncal@gmail.com>
2023-05-13 18:42:38 +0300

    update : site model

--- v1/src/controllers/site.controller.js
+++ v1/src/controllers/site.controller.js
@@ -2,6 +2,28 @@ const { list, insert } = require("../services/site.service");
 
 const create = async (req, res) => {
   const site = await insert(req.body);
+
+  /* const { site, siteBound, rp } = req.body;
+  const siteToInsert = await insertSite(site);
+  const siteBoundToInsert = await insertSiteBound({
+    ...siteBound,
+    site: siteToInsert._id,
+  });
+  const rpToInsert = await insertRP({
+    ...rp,
+    siteBound: siteBoundToInsert._id,
+  });
+
+  if (!siteToInsert || !siteBoundToInsert || !rpToInsert)
+    throw new Error("Site not created");
+  res.send({
+    site: siteToInsert,
+    siteBound: siteBoundToInsert,
+    rp: rpToInsert,
+    success: true,
+    message: "Site created successfully",
+  }); */
+
   res.send({
     site,
     success: true,
