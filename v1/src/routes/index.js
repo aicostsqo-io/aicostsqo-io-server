a8aeba3cec54ee20c77d8495d986f9c135bcddf8
mustafahincal <mustafahncal@gmail.com>
2023-11-23 13:47:37 +0300

    feat: other measurement technicals routes

--- v1/src/routes/index.js
+++ v1/src/routes/index.js
@@ -9,6 +9,18 @@ router.use('/siteBounds', authenticateToken, require('./siteBounds.routes'));
 router.use('/fields', authenticateToken, require('./sites.routes'));
 router.use('/rps', authenticateToken, require('./rps.routes'));
 router.use('/gprs', authenticateToken, require('./gprs.routes'));
+router.use(
+  '/magnetometrics',
+  authenticateToken,
+  require('./magnetometrics.routes')
+);
+router.use(
+  '/resistivities',
+  authenticateToken,
+  require('./resistivities.routes')
+);
+router.use('/seismics', authenticateToken, require('./seismics.routes'));
+router.use('/televiewers', authenticateToken, require('./televiewers.routes'));
 router.use('/rp-discs', require('./rpDiscs.routes'));
 
 module.exports = router;
