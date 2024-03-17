a2c8b104298002f73316360fad02c641df69de30
mustafahincal <mustafahncal@gmail.com>
2024-03-17 18:54:06 +0300

    refactor: pass jwt for mobile

--- v1/src/middlewares/authenticate.js
+++ v1/src/middlewares/authenticate.js
@@ -1,12 +1,15 @@
-const JWT = require("jsonwebtoken");
+const JWT = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-  const token = req.headers?.authorization?.split(" ")[1] || null;
-  if (token === null) throw new Error("You are not authorized.");
+  if (req.query.from === 'mobile') {
+    return next();
+  }
+  const token = req.headers?.authorization?.split(' ')[1] || null;
+  if (token === null) throw new Error('You are not authorized.');
 
   JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
-    if (err) throw new Error("Token expired.");
-    req.user = user["0"];
+    if (err) throw new Error('Token expired.');
+    req.user = user['0'];
     next();
   });
 };
