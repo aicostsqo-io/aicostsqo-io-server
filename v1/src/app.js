d6ea2d46470749b4242570203ae8b5c125e135e0
Vehbi Özcan <98270478+vehbiOzcan@users.noreply.github.com>
2024-04-19 21:04:27 +0300

    new uploads path app.js

--- v1/src/app.js
+++ v1/src/app.js
@@ -10,6 +10,9 @@ const dotenv = require('dotenv');
 const errorHandler = require('./middlewares/errorHandler');
 const notFound = require('./middlewares/notFound');
 
+const directory = path.join(__dirname, '../../../../../../aicostsqo-static/uploads');
+//console.log(directory);
+//console.log(__dirname);
 dotenv.config({
   path: './config/.env', //dotenv config içerisine config dosyamızın path ini verdik
 });
@@ -35,7 +38,10 @@ app.listen(process.env.APP_PORT, () => {
     res.send('Hello World');
   });
   app.use('/api', require('./routes'));
-  app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
+  //eski static directory
+  //app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
+  //yeni lokasyon  konfigurasyonu
+  app.use('/api/uploads', express.static(directory));
   app.use(notFound);
   app.use(errorHandler);
 });
